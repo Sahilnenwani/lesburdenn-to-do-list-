5c81dad80e2a1ac27ecb1d96e5f3d5db2af53aee
Sahil Nenwani <sk9941274@gmail.com>
2021-07-07 21:47:26 +0500

    Database Through firebase initialize

--- app.js
+++ app.js
@@ -5,11 +5,46 @@ const addtodobtn = document.getElementById("add-todo-btn");
 const delallbtn=document.getElementById('delete-all-todo')
 
 
+
+firebase.database().ref("to-do-item").on('child_added',(data)=>{
+  addtodolist(data.val().todoitem,data.val().key )  
+})
+
+function addtodolist(value,Id){
+  var li = document.createElement("li");
+  var litext = document.createTextNode(value);
+  li.appendChild(litext);
+
+  var div = document.createElement("div");
+  var delbtn = document.createElement("button");
+  var deltext = document.createTextNode("Delete");
+
+  delbtn.setAttribute("class", "btn btn-danger");
+  delbtn.setAttribute("id",Id);
+  delbtn.setAttribute("onclick", "deletetodo(this)");
+  var editbtn = document.createElement("button");
+  var editbtntext = document.createTextNode("Edit");
+  editbtn.setAttribute("class", "btn btn-success");
+  editbtn.setAttribute("id", Id);
+  editbtn.setAttribute("onclick", "edittodo(this)");
+  editbtn.appendChild(editbtntext);
+
+  delbtn.appendChild(deltext);
+  div.appendChild(delbtn);
+  div.appendChild(editbtn);
+  li.appendChild(div);
+  li.appendChild(div);
+  list.append(li);
+  delallbtn.classList.remove('for-hiding');
+
+}
+
+
 addtodobtn.addEventListener("click", () => {
  if (todolist.value==="") {
     Swal.fire({
         icon: 'info',
-        title: 'Please first enter any to-to',
+        title: 'Please first enter any to-do',
         showClass: {
           popup: 'animate__animated animate__fadeInDown'
         },
@@ -17,41 +52,22 @@ addtodobtn.addEventListener("click", () => {
           popup: 'animate__animated animate__fadeOutUp'
         }
       })
-  
-    // Swal.fire({
-    //     position: 'center',
-    //     icon: 'info',
-    //     title: 'Please first enter any to-to',
-    //     showConfirmButton: false,
-    //     timer: 1700
-    //   })    
+ 
  } 
  else{
-    var li = document.createElement("li");
-    var litext = document.createTextNode(todolist.value);
-    li.appendChild(litext);
-  
-    var div = document.createElement("div");
-    var delbtn = document.createElement("button");
-    var deltext = document.createTextNode("Delete");
-  
-    delbtn.setAttribute("class", "btn btn-danger");
-    delbtn.setAttribute("onclick", "deletetodo(this)");
-    var editbtn = document.createElement("button");
-    var editbtntext = document.createTextNode("Edit");
-    editbtn.setAttribute("class", "btn btn-success");
-    editbtn.setAttribute("onclick", "edittodo(this)");
-    editbtn.appendChild(editbtntext);
-  
-    delbtn.appendChild(deltext);
-    div.appendChild(delbtn);
-    div.appendChild(editbtn);
-    li.appendChild(div);
-    li.appendChild(div);
-    list.append(li);
-    
-   
+  let key = firebase.database().ref().push().key;
 
+  var dataforfirebase={
+    todoitem: todolist.value,
+    key:key
+  }
+   
+  firebase.database().ref("to-do-item/"+key).set(dataforfirebase); 
+//  addtodolist(todolist.value)
+
+
+    
+  
     Swal.fire({
       position: 'top-end',
       icon: 'success',
@@ -65,13 +81,19 @@ addtodobtn.addEventListener("click", () => {
   
 
 });
+
 delallbtn.addEventListener('click',()=>{
     list.innerHTML="";
     delallbtn.classList.add('for-hiding');    
-})
+    firebase.database().ref("to-do-item").remove();
+  })
 
 
 const deletetodo = (e) => {
+  // if (list.childNodes ==1) {
+  //   delallbtn.classList.add('for-hiding');
+  // }
+  firebase.database().ref("to-do-item/"+e.id).remove(); 
   e.parentNode.parentNode.remove();
   
 };
@@ -93,6 +115,15 @@ const edittodo = (e) => {
     //   }
 
 
+    
   var edit = prompt("Eid the todo",e.parentNode.parentNode.firstChild.nodeValue);
   e.parentNode.parentNode.firstChild.nodeValue = edit;
+  var dataforfirebase={
+    todoitem: edit,
+    key:e.id
+  }
+
+
+  firebase.database().ref("to-do-item/"+e.id).set(dataforfirebase);
 };
+
